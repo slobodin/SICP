a8635485f64fc88eeb04f6de551d8fc947e74436
flaming0 <epiforce57@gmail.com>
2012-05-14 18:21:25 +0400

    ch1

--- ch1/ex1-16.scm
+++ ch1/ex1-16.scm
@@ -1,2 +1,12 @@
 ;; Example 1.16.
 
+;; TEST NOT WORKING PROPERLY
+(define (fast-exp b n)
+  (define (iter a counter)
+    (if (= counter 0)
+        a
+        (iter (* a b) (- counter 1))))
+  (iter 1 n))
+
+
+(fast-exp 2 5)
