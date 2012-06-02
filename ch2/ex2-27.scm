f8fd7ab69a5f794c4e22a7e73812a244136836ac
flaming0 <epiforce57@gmail.com>
2012-06-02 13:14:39 +0400

    ch2

--- ch2/ex2-27.scm
+++ ch2/ex2-27.scm
@@ -3,8 +3,29 @@
 (define x (list (list 1 2) (list 3 4)))
 x
 
-(reverse x)
-(list? x)
+(define (my-reverse lst)
+  (define (iter l result)
+    (if (null? l)
+        result
+        (iter (cdr l) 
+              (cons (car l) result))))
+  (iter lst ()))
 
+(my-reverse x)
+
+;; When constructing reverse list from 
+;; (car list-on-this-iteration) and result
+;; we need to check, whether list-on-this-iteration is list
+;; if true - deep-reverse it.
 (define (deep-reverse items)
-  
+  (define (iter l result)
+    (if (null? l)
+        result
+        (iter (cdr l)
+              (cons (if (list? (car l)) (deep-reverse (car l))
+                        (car l))
+                    result))))
+  (iter items ()))
+
+(deep-reverse x)
+                  
---
+++ ch2/ex2-28.scm
@@ -0,0 +1,6 @@
+;; Example 2.28
+
+(define x (list (list 1 2) (list 3 4)))
+x
+
+(list x x)
