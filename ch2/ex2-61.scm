e8463ddfe313512511fa986147608869a32c3bfc
flaming0 <epiforce57@gmail.com>
2012-06-13 17:32:48 +0400

    ch2

--- ch2/ex2-61.scm
+++ ch2/ex2-61.scm
@@ -1,3 +1,5 @@
+;; Example 2.61
+
 (define (element-of-set? x set)
   (cond ((null? set) #f)
         ((= x (car set)) #t)
@@ -13,8 +15,15 @@
               ((< x1 x2) (intersection-set (cdr set1) set2))
               ((< x2 x1) (intersection-set set1 (cdr set2)))))))
 
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
 (define set1 (list 1 2 3 4 5))
 (define set2 (list 3 4 5 6 7))
 
 (element-of-set? 5 set1)
 (intersection-set set1 set2)
+(adjoin-set 12 set1)
+(adjoin-set 2 set1)
--- ch2/ex2-62.scm
+++ ch2/ex2-62.scm
@@ -1,3 +1,5 @@
+;; Example 2.62
+
 (define (element-of-set? x set)
   (cond ((null? set) #f)
         ((= x (car set)) #t)
@@ -13,8 +15,15 @@
               ((< x1 x2) (intersection-set (cdr set1) set2))
               ((< x2 x1) (intersection-set set1 (cdr set2)))))))
 
+;(define (union-set set1 set2)
+  ;(cond ((null? set1) set2)
+        ;((null? set2) set1)
+        ;(else (let ((x1 (car set1))
+                 ;   (x2 (car set2)))
+                ;(cond 
+
 (define set1 (list 1 2 3 4 5))
 (define set2 (list 3 4 5 6 7))
 
-(element-of-set? 5 set1)
+;(element-of-set? 5 set1)
 (intersection-set set1 set2)
