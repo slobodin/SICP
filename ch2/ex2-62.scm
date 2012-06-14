b265b56e4274c6331b27b4493b0f8daf8ff92192
flaming0 <epiforce57@gmail.com>
2012-06-14 12:50:30 +0400

    ch2

--- ch2/ex2-62.scm
+++ ch2/ex2-62.scm
@@ -15,15 +15,18 @@
               ((< x1 x2) (intersection-set (cdr set1) set2))
               ((< x2 x1) (intersection-set set1 (cdr set2)))))))
 
-;(define (union-set set1 set2)
-  ;(cond ((null? set1) set2)
-        ;((null? set2) set1)
-        ;(else (let ((x1 (car set1))
-                 ;   (x2 (car set2)))
-                ;(cond 
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else (let ((x1 (car set1))
+                    (x2 (car set2)))
+                (cond ((= x1 x2) (cons x1 (union-set (cdr set1) (cdr set2))))
+                      ((< x1 x2) (cons x1 (union-set (cdr set1) set2)))
+                      ((< x2 x1) (cons x2 (union-set set1 (cdr set2)))))))))
 
 (define set1 (list 1 2 3 4 5))
 (define set2 (list 3 4 5 6 7))
 
-;(element-of-set? 5 set1)
+(element-of-set? 5 set1)
 (intersection-set set1 set2)
+(union-set set1 set2)
---
+++ ch2/test_set_3.scm
@@ -0,0 +1,30 @@
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((= x (entry set)) #t)
+        ((< x (entry set)) (element-of-set x (left-branch set)))
+        (else (element-of-set x (right-branch set)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set)
+                    (adjoin-set x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set)
+                    (left-branch set)
+                    (adjoin-set x (right-branch set))))))
+
+(define t (make-tree 2 '() '()))
+(define t1 (adjoin-set 5 t))
+(define t2 (adjoin-set 3 t1))
+(define t3 (adjoin-set 1 t2))
+t3
