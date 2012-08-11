3eba47e6894ef0a97a868ded72679a70fe87fad5
flaming0 <epiforce57@gmail.com>
2012-08-11 16:00:17 +0400

    ch3

---
+++ ch3/ex3-38.scm
@@ -0,0 +1,10 @@
+;; Example 3.38
+
+;; balance - 100
+;; peter: (set! balance (+ balance 10))
+;; paul: (set! balance (- balance 20))
+;; mary: (set! balance (- balance (/ balance 2)))
+
+;; a)
+;; 
+;; 45, 40, 50, 35
