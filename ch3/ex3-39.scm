2ffec6bdc0a5e0a7d2bf599b37a1c076dd9bff53
flaming0 <epiforce57@gmail.com>
2012-08-18 12:56:09 +0400

    ch3

---
+++ ch3/ex3-39.scm
@@ -0,0 +1,6 @@
+;; Example 3.39
+
+(define x 10)
+(define s (make-serializer))
+(parallel-execute (lambda () (set! x ((s (lambda () (* x x))))))
+                  (s (lambda () (set! x (+ x 1)))))
--- ch3/test_concurrency.scm
+++ ch3/test_concurrency.scm
@@ -2,9 +2,44 @@
 ;; http://planet.racket-lang.org/display.ss?package=sicp-concurrency.plt&owner=dyoo
 ;; install package, reboot drracket, change language to "SICP Concurrency language"
 
-(define x 10)
+;; 121 101 11 100 110
 
-(parallel-execute (lambda () (set! x (* x x)))
-                  (lambda () (set! x (+ x 1))))
+(define (test)
+  (define (iter count)
+    (define x 10)
+    (if (<= count 0)
+        #t
+        (begin (parallel-execute (lambda () (set! x (* x x)))
+                                 (lambda () (set! x (+ x 1))))
+               (display x)
+               (newline)
+               (iter (- count 1)))))
+  (iter 40))
+
+(test)
+
+(define x 10)
+(define s (make-serializer))
+(parallel-execute (s (lambda () (set! x (* x x))))
+                  (s (lambda () (set! x (+ x 1)))))
 
 x
+
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds\n"))
+  
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  
+  (let ((protected (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) (protected withdraw))
+            ((eq? m 'deposit) (protected deposit))
+            ((eq? m 'balance) balance)
+            (else (error "Unknown request" m))))
+    dispatch))
