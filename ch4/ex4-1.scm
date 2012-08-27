5956bce0fd32ab1f71584b6570be3018e39a69ed
flaming0 <epiforce57@gmail.com>
2012-08-27 19:13:32 +0400

    ch4

--- ch4/ex4-1.scm
+++ ch4/ex4-1.scm
@@ -1,1 +1,19 @@
 #lang racket
+
+;; Example 4.1
+
+;; very not sure
+
+(define (list-of-values-l exps env)
+  (if (no-operands? exps)
+      '()
+      (let* ((first (eval (first-operand exps) env))
+             (rest (list-of-values-l (rest-operands exps) env)))
+        (cons first rest))))
+
+(define (list-of-values-r exps env)
+  (if (no-operands? exps)
+      '()
+      (let* ((rest (list-of-values-r (rest-operands exps) env))
+             (first (eval (first-operand exps) env)))
+        (cons rest first))))
---
+++ ch4/ex4-2.scm
@@ -0,0 +1,5 @@
+#lang racket
+
+;; Example 4.2
+
+;; trying to apply (define) func. But define is the special case.
